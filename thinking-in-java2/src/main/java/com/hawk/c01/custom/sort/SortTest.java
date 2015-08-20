c46536959bcf79a2f7770db80617c008868a69e6
larrychen1990 <larrychen1990@163.com>
2015-08-20 18:30:37 +0800

    sort

--- thinking-in-java2/src/main/java/com/hawk/c01/custom/sort/SortTest.java
+++ thinking-in-java2/src/main/java/com/hawk/c01/custom/sort/SortTest.java
@@ -1,6 +1,8 @@
 package com.hawk.c01.custom.sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.FixMethodOrder;
 import org.junit.Ignore;
@@ -9,6 +11,12 @@ import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.JVM)
 public class SortTest {
+
+	@Test
+	@Ignore
+	public void synthesizedSort() {
+
+	}
 
 	@Test
 	@Ignore
@@ -35,6 +43,7 @@ public class SortTest {
 	}
 
 	@Test
+	@Ignore
 	public void binaryInsertSort() {
 		int[] a = { 49, 38, 65, 97, 176, 213, 227, 49, 78, 34, 12, 164, 11, 18, 1 };
 
@@ -53,12 +62,12 @@ public class SortTest {
 				}
 			}
 
-			for (int j = i - 1; j >= left; j--) {	// move back 1 if neccesary
+			for (int j = i - 1; j >= left; j--) { // move back 1 if neccesary
 				a[j + 1] = a[j];
 			}
 
 			if (left != i) {
-				a[left] = temp;		// insert current value into the binary left
+				a[left] = temp; // insert current value into the binary left
 			}
 
 		}
@@ -126,6 +135,68 @@ public class SortTest {
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i] + ",");
 		}
+	}
+
+	@Test
+	@Ignore
+	public void heapSort() {
+		int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34,
+						15, 35, 25, 53, 51 };
+
+		int arrayLength = a.length;
+		// 循环建堆
+		for (int i = 0; i < arrayLength - 1; i++) {
+			// 建堆
+			buildMaxHeap(a, arrayLength - 1 - i);
+			// 交换堆顶和最后一个元素
+			swap(a, 0, arrayLength - 1 - i);
+			System.out.println(Arrays.toString(a));
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + ",");
+		}
+	}
+
+	private void swap(int[] data, int i, int j) {
+		int tmp = data[i];
+		data[i] = data[j];
+		data[j] = tmp;
+	}
+
+	// 对data数组从0到lastIndex建大顶堆
+	private void buildMaxHeap(int[] data, int lastIndex) {
+
+		// 从lastIndex处节点（最后一个节点）的父节点开始
+		for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
+			// k保存正在判断的节点
+			int k = i;
+			// 如果当前k节点的子节点存在
+			while (2 * k + 1 <= lastIndex) {
+				// k节点的左子节点的索引
+				int biggerIndex = 2 * k + 1;
+				// 如果biggerIndex小于lastIndex，即biggerIndex+1代表的k节点的右子节点存在
+				if (biggerIndex < lastIndex) {
+					// 如果右子节点的值较大
+					if (data[biggerIndex] < data[biggerIndex + 1]) {
+						// biggerIndex总是记录较大子节点的索引
+						biggerIndex++;
+					}
+				}
+				// 如果k节点的值小于其较大的子节点的值
+				if (data[k] < data[biggerIndex]) {
+					// 交换他们
+					swap(data, k, biggerIndex);
+					// 将biggerIndex赋予k，开始while循环的下一次循环，重新保证k节点的值大于其左右子节点的值
+					k = biggerIndex;
+				} else {
+					break;
+				}
+
+			}
+
+		}
+
 	}
 
 	@Test
@@ -259,11 +330,78 @@ public class SortTest {
 	}
 
 	@Test
+	public void radixSort() {
+		int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 101, 56, 17, 18,
+						23, 34, 15, 35, 25, 53, 51 };
+
+		// 首先确定排序的趟数; 找到最大的数
+		int max = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] > max) {
+				max = a[i];
+			}
+		}
+
+		// 判断最大数max有几位;
+		int times = 0;
+		while (max > 0) {
+			max /= 10;
+			times++;
+		}
+		
+		// 建立10个队列;
+		List<ArrayList<Integer>> queue = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < 10; i++) {
+			ArrayList<Integer> queue1 = new ArrayList<Integer>();
+			queue.add(queue1);
+		}
+
+		// 进行time次分配和收集
+		for (int i = 0; i < times; i++) {
+			
+			// 分配;
+			for (int j = 0; j < a.length; j++) {
+				// 得到数字的第time+1位数;
+				int x = a[j] % (int) Math.pow(10, i + 1) / (int) Math.pow(10, i);
+
+				ArrayList<Integer> queue2 = queue.get(x);
+				queue2.add(a[j]);
+				queue.set(x, queue2);
+			}
+			
+			int count = 0;
+			// 收集;
+			for (int k = 0; k < 10; k++) {
+				while (queue.get(k).size() > 0) {
+					ArrayList<Integer> queue3 = queue.get(k);
+					a[count] = queue3.get(0);
+					queue3.remove(0);
+					count++;
+				}
+			}
+
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + ",");
+		}
+	}
+
+	@Test
 	@Ignore
 	public void tt() {
 		double a = 5.0 / 2;
 		System.out.println(a);
 		System.out.println(Math.ceil(a));
+
+		int time = 0;
+		int max = 132;
+		while (max > 0) {
+			max /= 10;
+			time++;
+		}
+		System.out.println(max);
+		System.out.println(time);
 
 	}
 }
