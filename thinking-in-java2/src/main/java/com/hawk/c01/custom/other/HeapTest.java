97dd2d3b850d14aad88e1de2e745caaa1658f641
larrychen1990 <larrychen1990@163.com>
2015-09-09 17:07:07 +0800

    heap memory analyzer

---
+++ thinking-in-java2/src/main/java/com/hawk/c01/custom/other/HeapTest.java
@@ -0,0 +1,15 @@
+package com.hawk.c01.custom.other;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapTest {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		while (1 < 2) {
+			list.add("OutOfMemoryError soon");
+		}
+
+	}
+}
